bffd5c983d77831520c19cfae039f38ec0b8a313
Gustavo Santana Coelho <gustavo_13roo@hotmail.com>
2018-08-29 23:05:37 -0400

    Estudando aba da nota fiscal

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sqlite3 as database
 import select
 from app.NFCParser import NFCParser
 from app.business.NotaBO import NotaBO
+from pprint import *
 
 url_completa = 'https://www.sefaz.mt.gov.br/nfce/consultanfce?chNFe=51171047508411155940650690001703751000144339&nVersao=100&tpAmb=1&dhEmi=323031372d31302d30325431383a32313a34382d30343a3030&vNF=99.90&vICMS=0.00&digVal=726c564737336a667576387665673339676d77705547772b3569673d&cIdToken=000001&cHashQRCode=98BD5C7C79F7B01E5EF5A6365E741D4B1C7718AA'
 
@@ -17,28 +18,44 @@ url_varios_itens = 'https://www.sefaz.mt.gov.br/nfce/consultanfce?chNFe=51171109
 url_teste = 'https://www.sefaz.mt.gov.br/nfce/consultanfce?chNFe=51180109477652000943651040000195201090942311&nVersao=100&tpAmb=1&dhEmi=323031382d30312d33315431393a34323a33312d30333a3030&vNF=40.60&vICMS=0.00&digVal=79326a2f716f654854686c4d3445774a337959336f50706d4374303d&cIdToken=000001&cHashQRCode=15ADC327DBAA8871AAB213228C59D32CCC6C2A4F'
 
 def main ():
-    conexao = database.connect('db.db')
-    file = open('parsed.txt', 'r')
-    notaBo = NotaBO (conexao)
+	conexao = database.connect('db.db')
+	file = open('parsed.txt', 'r')
+	notaBo = NotaBO (conexao)
 
-    try:
-        for linha in file:
-            parser = NFCParser()
-            if linha.startswith('http://', 0, len('http://')):
-                linha = linha.replace('http://', 'https://')
-            linha = linha.strip(' \n')
-            r = requests.get(linha)
-            #print (r.text)
-            parser.feed(r.text)
-            #parser.dados['url'] = linha
-            #notaBo.write(parser.dados)
-            print(parser.dados)
-            break
-    except Exception as e:
-        print (e)
-    
-        
+	try:
+		for linha in file:
+			parser = NFCParser()
+			if linha.startswith('http://', 0, len('http://')):
+				linha = linha.replace('http://', 'https://')
+			linha = linha.strip(' \n')
+			r = requests.get(linha)
+			cookies = r.headers['Set-Cookie'].split(',')
 
-    
+			#pprint(r.headers)
+			
+			headers = []
+			for cook in cookies:
+				cookie = cook
+				cookie = cookie.replace("'", '')
+				if cookie != '':
+					headers.append({'Cookie', cookie})
+
+			pprint(headers)
+
+			r = requests.get('https://www.sefaz.mt.gov.br/nfce/consultanfce?pagn=visuAbas&tagSolicitada=1', headers)
+			#r = requests.get('https://www.sefaz.mt.gov.br/nfce/consultanfce?pagn=visuAbas&montaAba=true&tagSolicitada=1&ajaxRequest=true')
+
+			print (r.text)
+			#parser.feed(r.text)
+			#parser.dados['url'] = linha
+			#notaBo.write(parser.dados)
+			#print(parser.dados)
+			break
+	except Exception as e:
+		print (e)
+	
+		
+
+	
 
 main ()
